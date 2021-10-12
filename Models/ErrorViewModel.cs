5a3d5211dc165b09778cb348682c5cbac62e5742
Teresa Atencio <teresa.atencio.robles@hotmail.com>
2021-10-12 17:32:02 -0500

    mvc inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace S11_PC3_Simulacro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
